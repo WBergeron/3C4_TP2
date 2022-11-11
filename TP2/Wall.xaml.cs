1554e49d8c53dfc4261f0d01d5a897cf84c7b117
Julius Leblanc <1974350@cstj.qc.ca>
2022-11-11 14:15:56 -0500

    Début du projet
    Le Main est terminé et les fichiers Wall et Marketplace on été créé. Aussi, j'ai commencer ma partie qui est de faire le Wall

--- TP2/Wall.xaml.cs
+++ TP2/Wall.xaml.cs
@@ -10,28 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace TP2
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Logique d'interaction pour Wall.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Wall : Window
     {
-        public MainWindow()
+        public Wall()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click_Wall(object sender, RoutedEventArgs e)
-        {
-            new Wall().Show();
-        }
-        private void Button_Click_Marketplace(object sender, RoutedEventArgs e)
-        {
-            new Marketplace().Show();
         }
     }
 }
