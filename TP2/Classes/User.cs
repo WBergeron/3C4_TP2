59195b043cafa0fd0bc5f78ec1307c06c6afe4b4
William(2052824) <2052824@cstj.qc.ca>
2022-12-18 14:45:56 -0500

    Ajustement mineure pour fonctionnement

--- TP2/Classes/User.cs
+++ TP2/Classes/User.cs
@@ -14,11 +14,11 @@ namespace TP2.Classes
         public string LastName { get; set; }
         public string Password { get; set; }
         public string Courriel { get; set; }
-        public string SourceProfil { get; set; }
+        public string Source { get; set; }
         public string SourceBannerProfil { get; set; }
 
 
-        public BitmapImage Profil => new BitmapImage(App.GetUri(SourceProfil));
+        public BitmapImage Profil => new BitmapImage(App.GetUri(Source));
         public BitmapImage BannerProfil => new BitmapImage(App.GetUri(SourceBannerProfil));
         public override string ToString()
         {
